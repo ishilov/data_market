e3b6ed2bd10e55cd7b2451c19656c0d940abf348
Ilia <ilia.shilov@inria.fr>
2022-07-22 11:35:44 +0200

    tested aggregation

--- data_market.py
+++ data_market.py
@@ -1,16 +1,16 @@
 import numpy as np
 from scipy import stats
-import matplotlib as plt
+import matplotlib.pyplot as plt 
 
 class Seller:
     def __init__(self, probabilities: np.array, values: np.array, wager: float) -> None:
 
         self.forecast = [probabilities, values]
         self.wager = wager
-        self.distribution = self._distribution
+        self.distribution = self._distribution()
 
     def _distribution(self):
-        dist = stats.rv_histogram([self.probabilities, self.values])
+        dist = stats.rv_histogram([self.forecast[0], self.forecast[1]])
 
         return dist
 
@@ -24,22 +24,22 @@ class MarketOperator:
         pass
 
     @staticmethod
-    def plot_forecasts(*sellers):
+    def plot_forecasts(sellers: list):
         fig, ax = plt.subplots()
-
+        
         for seller in sellers:
             seller._plot_forecast()
 
         fig;
 
     @staticmethod
-    def _plot_aggregation(*sellers, agg_forecast):
+    def _plot_aggregation(sellers: list, agg_forecast):
         supp = np.sort(np.concatenate([np.linspace(*seller.distribution.support(), 1000) for seller in sellers]))
 
-        plt.plot(supp, agg_forecast(supp))
+        plt.plot(supp, agg_forecast(supp));
 
     @staticmethod
-    def aggregation(*sellers, type = 'QA', plot = False) -> np.array:
+    def aggregation(sellers: list, type = 'QA', plot = False):
         wager_sum = sum([seller.wager for seller in sellers])
 
         for seller in sellers:
@@ -56,13 +56,9 @@ class MarketOperator:
                     return aggregated_ppf(x)
 
             rv = aggregated_rv()
-
             agg_data = rv.rvs(size=1000000)
-
-            probas_res, values_res, _ = plt.hist(agg_data, bins = 200)
-
+            probas_res, values_res = np.histogram(agg_data, bins = 200, density=True)
             result_rv = stats.rv_histogram([probas_res, values_res])
-
             aggregated_forecast = result_rv.pdf
 
         if plot:
