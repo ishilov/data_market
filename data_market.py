ae2055c38209a6421a427b3289b394c39ba3c0b7
Ilia <ilia.shilov@inria.fr>
2022-07-25 23:12:29 +0200

    Rewritten market + scaling tests

--- data_market.py
+++ data_market.py
@@ -1,3 +1,4 @@
+from functools import partial
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt 
@@ -19,70 +20,102 @@ class Seller:
 
         plt.plot(support, self.forecast_rv.pdf(support))
 
+class Buyer:
+    def __init__(self, probabilities: np.array, values: np.array, utility) -> None:
+        self.forecast = [probabilities, values]
+        self.base_forecast = stats.rv_histogram([self.forecast[0], self.forecast[1]])
+        self.utility = utility
+
 class MarketOperator:
-    def __init__(self) -> None:
-        pass
 
     @staticmethod
-    def plot_forecasts(sellers: list, scale = True):
-        fig, ax = plt.subplots()
-        
-        if scale:
-            MarketOperator.scaler(sellers)
+    def _scale_distribution(values, probabilities, support):
+        scaled_values = (values - support[0]) / (support[-1] - support[0])
+        scaled_distribution = stats.rv_histogram([probabilities, scaled_values])
 
-        for seller in sellers:
+        return scaled_distribution
+
+    @staticmethod
+    def _task_indicator(task, support: np.array) -> np.array:
+        return support >= task
+
+    @staticmethod
+    def _scoring(forecast_rv, task, type = 'CRPS') -> float:
+        if type == 'CRPS':
+            support = np.linspace(*forecast_rv.support(), 1000)
+            task_cdf = MarketOperator._task_indicator(task, support)
+            integrand = (forecast_rv.cdf(support) - task_cdf) ** 2
+
+            return np.trapz(integrand, x = support)
+
+class Market:
+    '''Each instance of Market class is defined by the list of sellers, buyer and the task. 
+    '''
+    def __init__(self, sellers: list[Seller], buyer: Buyer, task) -> None:
+        self.sellers = sellers
+        self.score_dict = {f'Seller #{id}' : 0 for id, seller in enumerate(sellers)}
+
+        total_support = [np.linspace(*seller.forecast_rv.support(), 1000) for seller in self.sellers]
+        total_support.append(np.linspace(*buyer.base_forecast.support(), 1000))
+        self.total_supp = np.sort(np.concatenate(total_support))
+
+        self.scaled_dict = self._scale_forecasts()
+        self.task = task
+        self.buyer = buyer
+
+    def _scale_forecasts(self):
+        scaled_dict = {f'Seller #{id}' : MarketOperator._scale_distribution(values = seller.forecast[1], 
+                                                                            probabilities = seller.forecast[0], 
+                                                                            support= self.total_supp) for id, seller in enumerate(self.sellers)}
+
+        return scaled_dict
+
+    def plot_forecasts(self):
+        fig, ax = plt.subplots()
+
+        for seller in self.sellers:
             seller._plot_forecast()
 
         fig;
 
-    @staticmethod
-    def scaler(sellers):
-        #Rewrite this whole functionality
+    def plot_scaled_forecasts(self):
+        fig, ax = plt.subplots()
 
-        supp = np.sort(np.concatenate([np.linspace(*seller.forecast_rv.support(), 1000) for seller in sellers]))
+        for distribution in self.scaled_dict.values():
+            support = np.linspace(*distribution.support(), 1000)
 
-        for seller in sellers:
-            scaled_values = (seller.forecast[1] - supp[0]) / (supp[-1] - supp[0])
-            
-            seller.non_scaled_forecast_rv = seller.forecast_rv
-            seller.forecast_rv = stats.rv_histogram([seller.forecast[0], scaled_values])
+            plt.plot(support, distribution.pdf(support))
 
+        fig;
 
-    @staticmethod
-    def _plot_aggregation(sellers: list, agg_forecast):
-        supp = np.sort(np.concatenate([np.linspace(*seller.forecast_rv.support(), 1000) for seller in sellers]))
+    def _plot_aggregation(self, agg_forecast):
 
-        if agg_forecast.support() == (0,1):
-            supp = np.linspace(0,1, 1000)
+        supp = np.linspace(*agg_forecast.support(), 1000)
 
         plt.plot(supp, agg_forecast.pdf(supp));
 
-    @staticmethod
-    def aggregation(sellers: list, type = 'QA', plot = False, scale = True):
-        '''Returns a random variable, which is an instance of of stats.rv_continuous class'''
-        if scale:
-            MarketOperator.scaler(sellers)
-
-        wager_sum = sum([seller.wager for seller in sellers])
-
-        for seller in sellers:
-            seller.partial_wager = seller.wager / wager_sum
+    def scaled_aggregation(self, type = 'QA', plot = False):
+        wager_sum = sum([seller.wager for seller in self.sellers]) 
+        partial_wagers = [seller.wager / wager_sum for seller in self.sellers]
 
         if type == 'LOP':
-            aggregated_forecast_pdf = lambda x: sum([seller.partial_wager * seller.forecast_rv.pdf(x) for seller in sellers])
-            supp = np.sort(np.concatenate([np.linspace(*seller.forecast_rv.support(), 1000) for seller in sellers]))
+            aggregated_forecast_pdf = lambda x: sum([partial_wagers[id] * self.scaled_dict[f'Seller #{id}'].pdf(x) for id, seller in enumerate(self.sellers)])
 
             class aggregated_rv(stats.rv_continuous):
                 def _pdf(self, x):
                     return aggregated_forecast_pdf(x)
 
-                def _get_support(self):
-                    return supp[0], supp[-1]
+                def _cdf(self, x):
+                    support = np.linspace(0, x, 1000)
+                    return np.trapz(self._pdf(support), x = support)
 
-            aggregated_forecast = aggregated_rv()
-    
+                def _get_support(self):
+                    return 0, 1
+
+            scaled_aggregated_forecast = aggregated_rv()
+
         if type == 'QA':
-            aggregated_ppf = lambda x: sum([seller.partial_wager * seller.forecast_rv.ppf(x) for seller in sellers])
+            aggregated_ppf = lambda x: sum([partial_wagers[id] * seller.forecast_rv.ppf(x) for id, seller in enumerate(self.sellers)])
 
             class aggregated_rv(stats.rv_continuous):
                 def _ppf(self, x):
@@ -92,69 +125,103 @@ class MarketOperator:
             agg_data = rv.rvs(size=1000000)
             probas_res, values_res = np.histogram(agg_data, bins = 200, density=True)
 
-            #rewrite this scaling later
-            if scale:
-                values_res = (values_res - np.min(values_res)) / (np.max(values_res) - np.min(values_res))
+            #values_res = (values_res - np.min(values_res)) / (np.max(values_res) - np.min(values_res))
+            values_res = (values_res - self.total_supp[0]) / (self.total_supp[-1] - self.total_supp[0])
+
+            result_rv = stats.rv_histogram([probas_res, values_res])
+            scaled_aggregated_forecast = result_rv
+
+        if plot:
+            self._plot_aggregation(scaled_aggregated_forecast)
+
+        return scaled_aggregated_forecast
+
+
+    def aggregation(self, type = 'QA', plot = False):
+        '''Returns a random variable, which is an instance of of stats.rv_continuous class'''
+
+        wager_sum = sum([seller.wager for seller in self.sellers]) 
+        partial_wagers = [seller.wager / wager_sum for seller in self.sellers]
+
+        if type == 'LOP':
+            aggregated_forecast_pdf = lambda x: sum([partial_wagers[id] * seller.forecast_rv.pdf(x) for id, seller in enumerate(self.sellers)])
+            supp = self.total_supp
+
+            class aggregated_rv(stats.rv_continuous):
+                def _pdf(self, x):
+                    return aggregated_forecast_pdf(x)
+
+                def _cdf(self, x):
+                    support = np.linspace(0, x, 1000)
+                    return np.trapz(self._pdf(support), x = support)
+
+                def _get_support(self):
+                    return supp[0], supp[-1]
+
+            aggregated_forecast = aggregated_rv()
+    
+        if type == 'QA':
+            aggregated_ppf = lambda x: sum([partial_wagers[id] * seller.forecast_rv.ppf(x) for id, seller in enumerate(self.sellers)])
+
+            class aggregated_rv(stats.rv_continuous):
+                def _ppf(self, x):
+                    return aggregated_ppf(x)
+
+            rv = aggregated_rv()
+            agg_data = rv.rvs(size=1000000)
+            probas_res, values_res = np.histogram(agg_data, bins = 200, density=True)
+
 
             result_rv = stats.rv_histogram([probas_res, values_res])
             aggregated_forecast = result_rv
 
         if plot:
-            MarketOperator._plot_aggregation(sellers, aggregated_forecast)
+            self._plot_aggregation(aggregated_forecast)
 
         return aggregated_forecast
 
-    @staticmethod
-    def _task_indicator(task, support: np.array) -> np.array:
-        return support >= task
+    def _scale_task(self):
+        self.scaled_task = (self.task - self.total_supp[0]) / (self.total_supp[-1] - self.total_supp[0])
 
-    @staticmethod
-    def scoring(forecast_rv, task, type = 'CRPS') -> float:
-        if type == 'CRPS':
-            support = np.linspace(*forecast_rv.support(), 1000)
-            task_cdf = MarketOperator._task_indicator(task, support)
-            integrand = (forecast_rv.cdf(support) - task_cdf) ** 2
+    def make_scaling(self):
+        self._scale_forecasts()
+        self._scale_task()
 
-            return np.trapz(integrand, x = support)
+    def make_scoring(self):
+        for id, seller in enumerate(self.sellers):
+            self.score_dict[f'Seller {id}'] = MarketOperator._scoring(self.scaled_dict[f'Seller #{id}'], self.scaled_task)
+            
 
-
-    @staticmethod
-    def _skill_component(sellers: list, task):
+    def _skill_component(self):
         list_skill_payoff = []
-        weigted_total_scoring = sum([seller.wager * MarketOperator.scoring(seller.forecast_rv, task)] for seller in sellers)
-        wager_sum = sum([seller.wager for seller in sellers])
+        weigted_total_scoring = sum([seller.wager * self.score_dict[f'Seller #{id}']] for id, seller in enumerate(self.sellers))
+        wager_sum = sum([seller.wager for seller in self.sellers])
 
-        for seller in sellers:
-            personal_score = MarketOperator.scoring(seller.forecast_rv, task)
+        for id, seller in enumerate(self.sellers):
+            personal_score = self.score_dict[f'Seller #{id}']
             payoff = seller.wager * (1 + personal_score - weigted_total_scoring / wager_sum)
             list_skill_payoff.append(payoff)
 
         return list_skill_payoff
 
-
-    @staticmethod
-    def _utililty_component(sellers:list, buyer, task):
+    def _utililty_component(self):
         list_utility_payoff = []
-        buyers_score = MarketOperator.scoring(buyer.base_forecast, task)
-        if buyer.utility > 0:
-            weigted_total_scoring = sum([seller.wager * MarketOperator.scoring(seller.forecast_rv, task)] for seller in sellers if MarketOperator.scoring(seller.forecast_rv, task) > buyers_score)
-            for seller in sellers:
-                personal_score = MarketOperator.scoring(seller.forecast_rv, task)
-                payoff = buyer.utility * (personal_score * seller.wager) / weigted_total_scoring if personal_score > buyers_score else 0
+        buyers_score = MarketOperator._scoring(MarketOperator._scale_distribution(self.buyer.base_forecast), self.scaled_task)
+        if self.buyer.utility > 0:
+            weigted_total_scoring = sum([seller.wager * self.score_dict[f'Seller #{id}']] for id, seller in enumerate(self.sellers)
+                                                                 if self.score_dict[f'Seller #{id}'] > buyers_score)
+            for seller in self.sellers:
+                personal_score = self.score_dict[f'Seller #{id}']
+                payoff = self.buyer.utility * (personal_score * seller.wager) / weigted_total_scoring if personal_score > buyers_score else 0
                 list_utility_payoff.append(payoff)
 
         return list_utility_payoff
 
-    @staticmethod
-    def payoff(sellers: list, buyer, task) -> np.array:
-        list_skill_payoff = np.array(MarketOperator._skill_component(sellers, task))
-        list_utility_payoff = np.array(MarketOperator._utililty_component(sellers, buyer, task))
+    def calculate_payoffs(self) -> np.array:
+        list_skill_payoff = np.array(self._skill_component())
+        list_utility_payoff = np.array(self._utililty_component())
 
         return list_skill_payoff + list_utility_payoff
 
-class Buyer:
-    def __init__(self, base_forecast, utility) -> None:
-        self.base_forecast = base_forecast
-        self.utility = utility
 
     
